ef22f6266b8ef6c9279aec47ed9502b4839f21e5
Shivam <95582408+Shivam-090@users.noreply.github.com>
2025-08-28 18:00:48 +0530

    Update Layout.jsx

--- frontend/src/pages/admin/Layout.jsx
+++ frontend/src/pages/admin/Layout.jsx
@@ -16,7 +16,7 @@ const Layout = () => {
   return (
    <>
    <div className='flex items-center justify-between py-2 h-[70px] px-4 sm:px-12 border-b border-gray-200'>
-    <img src={assets.logo} onClick{()=>navigate('/')} alt="Site logo" className='w-6 sm:w-10 cursor-pointer'  />
+    <img src={assets.logo} onClick={()=>navigate('/')} alt="Site logo" className='w-6 sm:w-10 cursor-pointer'  />
     <button onClick={logout} className='text-sm px-8 py-2 bg-primary text-white rounded-full cursor-pointer' > Logout</button>
    </div>
 
